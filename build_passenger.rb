64894b640f0101ce7693aa5b6ef649c536ee26dc
Stefan Kaes <stefan.kaes@xing.com>
2022-11-30 16:59:21 +0100

    updated passenger and ruby versions

--- build_passenger.rb
+++ build_passenger.rb
@@ -32,7 +32,7 @@ build_depends "libyaml-dev"
 build_depends "pkg-config"
 build_depends "zlib1g-dev"
 
-depends "logjam-ruby", ">= 3.1.2"
+depends "logjam-ruby", ">= 3.1.3"
 depends "apache2"
 if codename == "trusty"
   depends "apache2-mpm-worker"
